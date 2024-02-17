b1a2b362986c38a4d34d67bc54757a778d6121e7
PaulmFfrench-dev <paulmffrench@gmail.com>
2024-02-17 16:26:11 +0000

    Handling post requests

--- routes.py
+++ routes.py
@@ -9,7 +9,10 @@ import forms
 def index():
     return render_template('index.html')
 
-@app.route('/about')
+@app.route('/about', methods=['GET', 'POST'])
 def about():
     form = forms.AddTaskForm()
+    if form.validate_on_submit():
+        print('Submitted title', form.title.data)
+        return render_template('about.html', form=form, title=form.title.data)
     return render_template('about.html', form=form)

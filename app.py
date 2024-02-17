bc971b483b7458ae8b32a08910324ed79b160a86
PaulmFfrench-dev <paulmffrench@gmail.com>
2024-02-17 15:28:30 +0000

    Extending basic Jinja templating

--- app.py
+++ app.py
@@ -6,8 +6,11 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('index.html', current_title='Custom Title')
+    return render_template('index.html')
 
+@app.route('/about')
+def about():
+    return render_template('about.html')
 # name is main when executed from command line
 if __name__ == '__main__':
     app.run(debug=True) 
